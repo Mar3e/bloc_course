193f0f5e3d77516da71fde92c535414664a63347
Mar3e <mar3e.1420.1435@gmail.com>
2024-11-07 10:39:13 +0300

    first step Cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+import 'dart:math' as math show Random;
+
+const names = [
+  'John',
+  'Jane',
+  'Jack',
+  'Jill',
+  'Jerry',
+  'June',
+  'Jim',
+  'Jenny',
+  'Joe',
+  'Joan',
+];
+
+extension RandomElement<T> on Iterable<T> {
+  T get randomElement => elementAt(math.Random().nextInt(length));
+}
+
+class NameCubit extends Cubit<String?> {
+  NameCubit() : super(null);
+
+  void getRandomName() => emit(names.randomElement);
+}
 
 void main() {
   runApp(const MyApp());
@@ -25,8 +50,53 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late final NameCubit _nameCubit;
+
+  @override
+  void initState() {
+    super.initState();
+    _nameCubit = NameCubit();
+  }
+
+  @override
+  void dispose() {
+    _nameCubit.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Random'),
+      ),
+      body: Center(
+        child: StreamBuilder(
+          stream: _nameCubit.stream,
+          builder: (context, snapshot) {
+            final button = ElevatedButton(
+              onPressed: _nameCubit.getRandomName,
+              child: const Text('Get random name'),
+            );
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return button;
+              case ConnectionState.waiting:
+                return button;
+              case ConnectionState.active:
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(snapshot.data.toString()),
+                    button,
+                  ],
+                );
+              case ConnectionState.done:
+                return const SizedBox();
+            }
+          },
+        ),
+      ),
+    );
   }
 }
